1213d5eb401c83eab3578be6b53dfa87b3343037
Aisha1511 <aichamide1@gmail.com>
2024-06-28 23:34:34 +0000

    aisha

--- consts/consts.go
+++ consts/consts.go
@@ -13,11 +13,11 @@ import (
 
 const (
 	// TODO: choose a human-readable part for your hyperchain
-	HRP = ""
+	HRP = "aicha"
 	// TODO: choose a name for your hyperchain
-	Name = ""
+	Name = "mide"
 	// TODO: choose a token symbol
-	Symbol = ""
+	Symbol = "aisha"
 )
 
 var ID ids.ID
